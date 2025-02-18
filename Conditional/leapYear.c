a2360a561cdb220d3a32aea6f62f595cf96f1213
arjahid <jhjahidhasanar@gmail.com>
2025-02-18 21:23:36 +0600

    solve eligible for vote

--- Conditional/leapYear.c
+++ Conditional/leapYear.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int main()
+{
+  int a;
+  scanf("%d ",&a);
+  if(a%400 ==0 ||a%4==0 && a%100!=0){
+    printf("%d is leap year",a);
+  }else{
+    printf("%d is not leap year",a);
+  }
+}
