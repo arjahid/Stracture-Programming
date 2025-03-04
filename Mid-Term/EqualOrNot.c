28933a56464c7a09212ae10d5f057b15663d479a
arjahid <jhjahidhasanar@gmail.com>
2025-03-04 20:05:34 +0600

    solve

--- Mid-Term/EqualOrNot.c
+++ Mid-Term/EqualOrNot.c
@@ -2,9 +2,9 @@
 int main(){
     int a=15, b=15;
     if(a==b){
-        printf("a is equal to b|n";)
+        printf("a is equal to b\n");
     }
     else{
-        printf("a is not equal to b|n");
+        printf("a is not equal to b\n");
     }
 }
