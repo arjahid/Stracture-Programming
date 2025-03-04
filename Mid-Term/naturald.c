aac6c1010ed9a6fdbb4c7300d6689d839c19cacf
arjahid <jhjahidhasanar@gmail.com>
2025-03-04 23:19:50 +0600

    solve

---
+++ Mid-Term/naturald.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int i,n=7;
+    int sum=0;
+    for(i=1;i<=n:i++){
+        sum=sum+i;
+    }
+    printf("%d",sum);
+}
--- Mid-Term/sum.c
+++ Mid-Term/sum.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
     int i;
+    int sum=0;
     for(i=1;i<=10;i++){
-        printf("%d\n",i);
+        sum=sum+i;
     }
+    printf("%d",sum);
 }
