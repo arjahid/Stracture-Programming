76f6d5bdbe50d62c4e432351fb13f7ba5cb70f4c
arjahid <jhjahidhasanar@gmail.com>
2025-03-04 23:22:34 +0600

    solve

--- Mid-Term/naturald.c
+++ Mid-Term/naturald.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main(){
-    int i,n=7;
+    int i,n=10;
     int sum=0;
+    int avg=0;
     for(i=1;i<=n:i++){
         sum=sum+i;
+        avg=sum/n;
     }
     printf("%d",sum);
+    printf("%d",avg);
 }
