8ba8cfb6dbbc6c0bfae20bf7713f459aa66d4249
arjahid <jhjahidhasanar@gmail.com>
2025-03-12 14:19:16 +0600

    solve

---
+++ Array/String/alpha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+  char alphabet[] = "jahid";
+  printf("Length is: %d\n", strlen(alphabet));
+  printf("Size is: %d\n", sizeof(alphabet));
+  return 0;
+}
