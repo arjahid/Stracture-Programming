68144f6934e223d8b2f05e79721d8e38ec3a548b
arjahid <jhjahidhasanar@gmail.com>
2025-03-15 03:30:27 +0600

    getting all element of array using loop in ponter

--- memorAddres/getfelement.c
+++ memorAddres/getfelement.c
@@ -7,5 +7,8 @@ int main()
     int number[] = {2, 2, 3, 4, 5};
     printf("%d\n",number[0]);
     printf("%d", *number);
+    printf("%d", *number+1);
+    printf("%d", *number+2);
+    printf("%d", *number+3);
     // so we get first element of array by using *numbeer(pointer) also using number[0]
 }
