c2a1bf2cf89d5288491538839e3b2f0acaecb477
arjahid <jhjahidhasanar@gmail.com>
2025-03-15 03:20:51 +0600

    getting first element of array using poiner

--- memorAddres/getfelement.c
+++ memorAddres/getfelement.c
@@ -7,4 +7,5 @@ int main()
     int number[] = {2, 2, 3, 4, 5};
     printf("%d\n",number[0]);
     printf("%d", *number);
+    // so we get first element of array by using *numbeer(pointer) also using number[0]
 }
