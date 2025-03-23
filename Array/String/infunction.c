dee7a22089d1d367c95a30f17ae60d00e6fe50bc
arjahid <jhjahidhasanar@gmail.com>
2025-03-23 21:33:44 +0600

    solve

---
+++ Array/String/infunction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+void details(char name[]){
+  printf("%s\n",name);
+}
+int main()
+{
+    details("Jahid hasan");
+    details("Jahid hasan");
+    details("Jahid hasan");
+}
