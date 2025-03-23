5e684c373a2da42a77a5574abc1179c2f26b9a35
arjahid <jhjahidhasanar@gmail.com>
2025-03-23 21:29:11 +0600

    solve

---
+++ Array/String/actualc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+  char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  printf("Length is: %d\n", strlen(alphabet));
+  printf("Size is: %d\n", sizeof(alphabet));
+  return 0;
+}
