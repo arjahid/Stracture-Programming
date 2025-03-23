5ecc01ff64fe4f5d8cc6864247276e173972ca72
arjahid <jhjahidhasanar@gmail.com>
2025-03-23 21:11:26 +0600

    solve

---
+++ Function/newstr.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+   char name[]="jahid Hasan";
+   name[0]='J';
+   printf("%s\n",name);
+   printf("%c\n",name[0]);
+   printf("%c",name[1]);
+
+    return 0;
+}
